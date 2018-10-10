b5a200f606825f6497c8f92596fa332a1d5097cc
tania <pocketmehdi@gmail.com>
2018-10-10 09:51:47 +0300

    Add compare function

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
 import program from 'commander';
+import genDiff from '../compare';
 
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
   .arguments('<firstConfig> <secondConfig>')
+  .action((firstPath, secondPath) => console.log(genDiff(firstPath, secondPath)))
   .parse(process.argv);
